1a668b32cb0db5edb968a36f705de45170b20848
Matheus Viana de Oliveira <matheus.viana@hotmail.com>
2025-01-29 16:01:23 -0300

    Swagger added

--- src/main/java/com/transaction/desafio/controller/StatisticController.java
+++ src/main/java/com/transaction/desafio/controller/StatisticController.java
@@ -2,6 +2,8 @@ package com.transaction.desafio.controller;
 
 import com.transaction.desafio.dto.StatisticDTO;
 import com.transaction.desafio.service.StatisticService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ public class StatisticController {
     private StatisticService statisticService;
 
     @GetMapping
+    @Operation(description = "Endpoint responsible for obtaining transaction statistics from a range.")
+    @ApiResponse(responseCode = "200", description = "Statistics fetched with success.")
     public StatisticDTO getStatistics(@RequestParam("interval") Integer interval) {
         return statisticService.getStatistics(interval);
     }
--- src/main/java/com/transaction/desafio/controller/TransactionController.java
+++ src/main/java/com/transaction/desafio/controller/TransactionController.java
@@ -2,6 +2,9 @@ package com.transaction.desafio.controller;
 
 import com.transaction.desafio.dto.TransactionDTO;
 import com.transaction.desafio.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +18,19 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @PostMapping
+    @Operation(description = "Endpoint responsible for post transactions.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transaction created with success."),
+            @ApiResponse(responseCode = "422", description = "Error in request body.")
+    })
     public ResponseEntity<Void> postTransaction(@RequestBody TransactionDTO dto) {
         transactionService.addTransaction(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint responsible for delete all transactions.")
+    @ApiResponse(responseCode = "200", description = "Transactions deleted with success.")
     public ResponseEntity<Void> deleteTransactions() {
         transactionService.deleteTransactions();
         return ResponseEntity.ok().build();
