27400fc43d6fed8142746db68d52d1867bfe9fe2
Matheus Viana de Oliveira <matheus.viana@hotmail.com>
2025-01-29 16:10:31 -0300

    Logs added

--- src/main/java/com/transaction/desafio/service/StatisticService.java
+++ src/main/java/com/transaction/desafio/service/StatisticService.java
@@ -2,6 +2,7 @@ package com.transaction.desafio.service;
 
 import com.transaction.desafio.dto.StatisticDTO;
 import com.transaction.desafio.dto.TransactionDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +10,18 @@ import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
 @Service
+@Slf4j
 public class StatisticService {
 
     @Autowired
     private TransactionService transactionService;
 
     public StatisticDTO getStatistics(Integer searchInterval) {
+        log.info("Getting statistics from the last {} seconds", searchInterval);
         List<TransactionDTO> transactionsDTOS = transactionService.getTransactions(searchInterval);
         // transactions are not empty
         if (transactionsDTOS.isEmpty()) {
+            log.info("Empty transactions.");
             return new StatisticDTO(0L, 0.0, 0.0, 0.0, 0.0);
         }
         // study this
--- src/main/java/com/transaction/desafio/service/TransactionService.java
+++ src/main/java/com/transaction/desafio/service/TransactionService.java
@@ -2,6 +2,7 @@ package com.transaction.desafio.service;
 
 import com.transaction.desafio.dto.TransactionDTO;
 import com.transaction.desafio.exceptions.UnprocessableEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -9,34 +10,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class TransactionService {
 
     // data in memory
-    private List<TransactionDTO> transactions = new ArrayList<>();
+    private final List<TransactionDTO> transactions = new ArrayList<>();
 
     public void addTransaction(TransactionDTO transactionDTO) {
         transactionValidation(transactionDTO);
+        log.info("Transaction validated.");
         transactions.add(transactionDTO);
     }
 
     public void deleteTransactions() {
         transactions.clear();
+        log.info("Clear all transactions.");
     }
 
     public List<TransactionDTO> getTransactions(Integer seconds) {
-        OffsetDateTime gap = OffsetDateTime.now().minusSeconds(seconds);
+        OffsetDateTime searchInterval = OffsetDateTime.now().minusSeconds(seconds);
         return transactions
                 .stream()
-                .filter(transaction -> transaction.getDataHora().isAfter(gap))
+                .filter(transaction -> transaction.getDataHora().isAfter(searchInterval))
                 .toList();
     }
 
     // add transaction validation
     private void transactionValidation(TransactionDTO transactionDTO) {
+        log.info("Validating transaction...");
         if (transactionDTO.getDataHora().isAfter(OffsetDateTime.now())) {
+            log.error("Date and time greater than current.");
             throw new UnprocessableEntity("Data e hora maiores que atuais.");
         }
         if (transactionDTO.getValor() < 0) {
+            log.error("Negative transaction value.");
             throw new UnprocessableEntity("Valor da transação negativo.");
         }
     }
